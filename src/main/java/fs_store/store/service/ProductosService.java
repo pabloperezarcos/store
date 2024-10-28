e50e21d42bbf7b1fc51212ddb99bd669d7ad4166
pabloperezarcos <pabloperezarcos@gmail.com>
2024-10-28 20:47:05 -0300

    Update ProductosService.java

--- src/main/java/fs_store/store/service/ProductosService.java
+++ src/main/java/fs_store/store/service/ProductosService.java
@@ -1,5 +1,20 @@
 package fs_store.store.service;
 
-public class ProductosService {
+import fs_store.store.model.Productos;
+import java.util.List;
+import java.util.Optional;
 
+public interface ProductosService {
+
+    List<Productos> obtenerTodosLosProductos();
+
+    Optional<Productos> obtenerProductoPorId(int id);
+
+    Productos crearProducto(Productos producto);
+
+    Productos actualizarProducto(int id, Productos productoActualizado);
+
+    void eliminarProducto(int id);
+
+    Optional<Productos> obtenerProductoPorNombre(String nombre);
 }
