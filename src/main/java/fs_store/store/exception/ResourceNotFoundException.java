baee63aa61322b7eb1fc5d1d3c66f97683611ae2
pabloperezarcos <pabloperezarcos@gmail.com>
2024-12-08 19:05:59 -0300

    Create ResourceNotFoundException.java

---
+++ src/main/java/fs_store/store/exception/ResourceNotFoundException.java
@@ -0,0 +1,27 @@
+package fs_store.store.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+    private String resourceName;
+    private String fieldName;
+    private Object fieldValue;
+
+    public ResourceNotFoundException(String resourceName, String fieldName, Object fieldValue) {
+        super(String.format("%s no encontrado con %s : '%s'", resourceName, fieldName, fieldValue));
+        this.resourceName = resourceName;
+        this.fieldName = fieldName;
+        this.fieldValue = fieldValue;
+    }
+
+    // Getters
+    public String getResourceName() {
+        return resourceName;
+    }
+
+    public String getFieldName() {
+        return fieldName;
+    }
+
+    public Object getFieldValue() {
+        return fieldValue;
+    }
+}
