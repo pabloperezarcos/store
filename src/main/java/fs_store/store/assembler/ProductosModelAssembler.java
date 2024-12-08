ac16b14319cc2af94362769d60c857b55cb225ec
pabloperezarcos <pabloperezarcos@gmail.com>
2024-12-08 19:05:51 -0300

    Update ProductosModelAssembler.java

--- src/main/java/fs_store/store/assembler/ProductosModelAssembler.java
+++ src/main/java/fs_store/store/assembler/ProductosModelAssembler.java
@@ -3,28 +3,27 @@ package fs_store.store.assembler;
 import fs_store.store.controller.ProductosController;
 import fs_store.store.model.Productos;
 import org.springframework.hateoas.EntityModel;
-import org.springframework.hateoas.server.mvc.RepresentationModelAssemblerSupport;
+import org.springframework.hateoas.server.RepresentationModelAssembler;
 import org.springframework.stereotype.Component;
+import org.springframework.lang.NonNull;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 
 @Component
-public class ProductosModelAssembler extends RepresentationModelAssemblerSupport<Productos, EntityModel<Productos>> {
-
-    @SuppressWarnings("unchecked")
-    public ProductosModelAssembler() {
-        super(ProductosController.class, (Class<EntityModel<Productos>>) (Class<?>) EntityModel.class);
-    }
+public class ProductosModelAssembler implements RepresentationModelAssembler<Productos, EntityModel<Productos>> {
 
     @SuppressWarnings("null")
     @Override
-    public EntityModel<Productos> toModel(Productos producto) {
-        EntityModel<Productos> entityModel = EntityModel.of(producto);
-
-        entityModel
-                .add(linkTo(methodOn(ProductosController.class).obtenerProductoPorId(producto.getId())).withSelfRel());
-        entityModel.add(linkTo(methodOn(ProductosController.class).obtenerProductos()).withRel("productos"));
-
-        return entityModel;
+    public EntityModel<Productos> toModel(@NonNull Productos producto) {
+        return EntityModel.of(producto,
+                // Enlace a sí mismo
+                linkTo(methodOn(ProductosController.class).obtenerProductoPorId(producto.getId())).withSelfRel(),
+                // Enlace a la colección de productos
+                linkTo(methodOn(ProductosController.class).obtenerTodosLosProductos()).withRel("productos"),
+                // Enlace para actualizar el producto
+                linkTo(methodOn(ProductosController.class).actualizarProducto(producto.getId(), producto)).withRel("actualizar"),
+                // Enlace para eliminar el producto
+                linkTo(methodOn(ProductosController.class).eliminarProducto(producto.getId())).withRel("eliminar")
+        );
     }
 }
