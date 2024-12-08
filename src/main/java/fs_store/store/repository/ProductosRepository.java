fbb8933078cbc0fee3b5a739efc225b25cbadd43
pabloperezarcos <pabloperezarcos@gmail.com>
2024-12-08 19:06:02 -0300

    Update ProductosRepository.java

--- src/main/java/fs_store/store/repository/ProductosRepository.java
+++ src/main/java/fs_store/store/repository/ProductosRepository.java
@@ -1,12 +1,17 @@
 package fs_store.store.repository;
 
+import fs_store.store.model.Productos;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import fs_store.store.model.Productos;
+
 import java.util.Optional;
 
 @Repository
 public interface ProductosRepository extends JpaRepository<Productos, Integer> {
-    // Método para buscar un producto por su nombre
+
+    // Método para buscar productos por SKU
+    Optional<Productos> findBySku(String sku);
+
+    // Método para buscar productos por Nombre
     Optional<Productos> findByNombre(String nombre);
 }
